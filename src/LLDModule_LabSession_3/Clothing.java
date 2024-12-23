bc7c5f7282c39cc637c3ed967b4fed11c073981a
Rahul Saple <rahul.saple@mobiotics.com>
2024-12-23 21:22:25 +0530

    LLD-Lab-3

---
+++ src/LLDModule_LabSession_3/Clothing.java
@@ -0,0 +1,17 @@
+package LLDModule_LabSession_3;
+
+public class Clothing extends Item{
+    private int size;
+
+    public Clothing(String name, int size){
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
---
+++ src/LLDModule_LabSession_3/Electronics.java
@@ -0,0 +1,16 @@
+package LLDModule_LabSession_3;
+
+public class Electronics extends Item{
+    private int warranty;
+
+    public Electronics(String id, String name, double price, int quantity, int warranty) {
+        super(id, name, price, quantity);
+        this.warranty = warranty;
+    }
+    public int getWarranty() {
+        return warranty;
+    }
+    public void setWarranty(int warranty) {
+        this.warranty = warranty;
+    }
+}
